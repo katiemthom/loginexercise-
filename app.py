1ba3803e1e1160a28c9dfbe902f45eecf2686300
Hackbright Student <user@mini-006.(none)>
2013-10-21 17:58:27 -0700

    initial commit

--- app.py
+++ app.py
@@ -6,15 +6,81 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "User %s is logged in! <a href='logout'>Logout</a>" % session["username"]
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    model.connect_to_db()
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    user_id = model.authenticate(username, password)
+
+    print "in app.py, user_id:", user_id
+
+    if user_id != None:
+        flash("User authenticated!")
+        session['username'] = user_id
+    else: 
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+
+
+    return redirect(url_for("index"))
+
+
+@app.route("/register")
 def register():
-    return render_template("register.html")
+    model.connect_to_db()
+    if session.get("username"):
+         # get username by id 
+         username = model.get_username_by_id(session["username"])
+         return redirect("users/"+username)
+    else:
+        return render_template("register.html")
+    #model.register_new_user(new_username, new_password)
+
+@app.route("/register", methods=["POST"])
+def create_account():
+    model.connect_to_db()
+    username = request.form.get("username")
+    password = request.form.get("password")
+    model.create_new_user(username, password)
+
+    # TODO automatic login once registration is done
+    
+    return redirect("user/" + username)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+@app.route("/user/<username>")
+def view_user(username):
+    model.connect_to_db()
+    posts = model.get_user_by_name(username)
+    user_id = session.get("username")
+    return render_template("wall.html", wall_posts = posts, user_id = user_id)
+
+# this grabs info on the wall of a user's wall
+@app.route("/user/<username>", methods=["POST"])
+def post_to_wall(username):
+    model.connect_to_db()
+
+    # this is a note of who is who
+    owner_username = username
+
+    # this is author that will be posting to the owners user wall
+    author_user_id = session.get("username")
+    new_post = request.form.get("post_content")
+
+    model.make_wall_post(author_user_id, owner_username, new_post)
+    return redirect("/user/" + username)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,92 @@
+import sqlite3
+from datetime import date
+
 ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+ADMIN_PASSWORD=5980025637247534551
+DB = None
+CONN = None
+
+def connect_to_db():
+    global CONN 
+    CONN = sqlite3.connect('thewall.db')
+    global DB 
+    DB = CONN.cursor()
 
 def authenticate(username, password):
-    return False
+    query = """SELECT id, username, password FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+
+    if username == row[1] and str(hash(password)) == row[2]:
+        return row[0]
+    else:
+        return None
+
+
+# uses user id to get all the wall posts from a user
+# author's name, created_at datetime, and then post content
+# username of the wall
+# get the posts
+# each post has an author_id
+# use author id to get author name 
+def get_user_by_name(username):
+    # join "users" and "wall_post" tables together based on the user_id of owner_id
+    # query = """SELECT wp.content, wp.created_at, owner.username, author.username FROM wall_posts AS wp
+    #         INNER JOIN users AS owner ON (owner.id = wp.owner_id)
+    #         INNER JOIN users AS author ON (author.id = wp.author_id)
+    #         WHERE owner.username = ?"""
+
+    query = """SELECT wp.content, wp.created_at, owner.username, author.username
+            FROM users AS author, users AS owner
+            INNER JOIN wall_posts AS wp ON (owner.id = wp.owner_id)
+            WHERE (wp.author_id = author.id) AND owner.username = ?"""
+
+    DB.execute(query, (username,))
+    rows = DB.fetchall()
+    return rows 
+
+######### HELPER FUNCTIONS ########
+def get_username_by_id(user_id):
+    query = """SELECT username 
+                FROM users
+                WHERE id=?"""
+    DB.execute(query, (user_id,))
+
+    return DB.fetchone()[0]
+
+
+def get_id_by_username(username):
+    query = """SELECT id
+                FROM users
+                WHERE username=?"""
+    DB.execute(query, (username,))
+    
+    return DB.fetchone()[0]
+####################################
+
+def make_wall_post(author_id, owner_username, content):
+    # grab the id of a owner by their username
+    owner_id = get_id_by_username(owner_username)
+
+    today_date = date.today()
+
+    # make a post providing all the information we need 
+    insert_query = """INSERT INTO wall_posts
+                    VALUES (NULL, ?, ?, ?, ?)"""
+    DB.execute(insert_query, (owner_id, author_id, today_date, content))
+    CONN.commit()
+    return 
+
+
+
+def create_new_user(username, password):
+    # use a hash password in case DB gets broken into
+    print "username", username
+    print "password", password
+    hash_password = str(hash(password))
+
+    new_user_query = """INSERT INTO users
+                        VALUES (NULL, ?, ?)"""
+    DB.execute(new_user_query, (username, hash_password))
+    CONN.commit()
+    return
